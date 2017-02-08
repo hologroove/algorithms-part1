efd0a556b45b147a46175d35a01424643714fad4
Laszlo Ivacs <laszlo.ivacs@ericsson.com>
2017-02-08 15:00:59 +0100

    First attempt of PercolationStats

--- Percolation.java
+++ Percolation.java
@@ -1,12 +1,10 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    
+
     //create n-by-n grid with all sites blocked
-    
-    
+
+
     private int height = 0;
     private int width  = 0;
     private int virtualTop = 0;
@@ -14,83 +12,93 @@ public class Percolation {
     private int[] field = null;
     private int numOfOpen = 0;
     private WeightedQuickUnionUF uf = null;
-    
+
     public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+
         //new array: 0 - n*n-1
         field = new int[n*n];
-        
+
         //set private height, width
         height = n;
-        width = n; //TODO: not needed        
-        
+        width = n; //TODO: not needed
+
         //create WeightedQuickUnionUF(1dsize + 2)
         //2 extra nodes will be used for virtual top and bottom
         uf = new WeightedQuickUnionUF(n*n+2);
         virtualTop = n*n;
         virtualBottom = n*n+1;
-        
+
         //connect virtual top to toprow
         //connect virtual bottom to bottom row
         for (int i=1; i<=n; i++) {
             uf.union(virtualTop, toArrayIndex(1, i));
             uf.union(virtualBottom, toArrayIndex(n, i));
-        }        
+        }
     }
-    
+
     //open site (row, col) if it is not open already
     public void open(int row, int col) {
-        //convert row, col to 1d index
-        int index = toArrayIndex(row, col);
-        //set field[index] to 1
-        field[index] = 1;
-        
-        //if not top row, union(index, index-1row)
-        if (row > 1) {
-            uf.union(index, toArrayIndex(row - 1, col));
+        //if it is not already open
+        if (isFull(row, col)) {
+            //convert row, col to 1d index
+            int index = toArrayIndex(row, col);
+            //set field[index] to 1
+            field[index] = 1;
+
+            //if not top row, union(index, index-1row)
+            if (row > 1) {
+                uf.union(index, toArrayIndex(row - 1, col));
+            }
+            //if not bottom row, union(index, index+1row)
+            if (row < height) {
+                uf.union(index, toArrayIndex(row + 1, col));
+            }
+            //if not 1st column, union(index, index-1)
+            if (col > 1) {
+                uf.union(index, toArrayIndex(row, col - 1));
+            }
+            //if not last column, union(index, index+1)
+            if (col < width) {
+                uf.union(index, toArrayIndex(row, col + 1));
+            }
+
+            numOfOpen++;
         }
-        //if not bottom row, union(index, index+1row)
-        if (row < height) {
-            uf.union(index, toArrayIndex(row + 1, col));
-        }
-        //if not 1st column, union(index, index-1)
-        if (col > 1) {
-            uf.union(index, toArrayIndex(row, col - 1));
-        }
-        //if not last column, union(index, index+1)
-        if (col < width) {
-            uf.union(index, toArrayIndex(row, col + 1));
-        }
-        
-        numOfOpen++;
     }
-    
+
     //is site (row, col) open?
     public boolean isOpen(int row, int col) {
         //if field[index] equals 1, then true
         return (field[toArrayIndex(row, col)] == 1) ? true : false;
     }
-    
+
     //is site (row, col) full?
     public boolean isFull(int row, int col) {
         return (field[toArrayIndex(row, col)] == 0) ? true : false;
     }
-    
+
     //number of open sites
     public int numberOfOpenSites() {
         return numOfOpen;
     }
-    
+
     //does the system percolate
     public boolean percolates() {
         return uf.connected(virtualTop, virtualBottom);
     }
-    
+
     //convert 2d-1d
     private int toArrayIndex(int row, int col) {
+        if (row < 1 || row > height || col < 1 || col > width) {
+            throw new IndexOutOfBoundsException();
+        }
         return (row-1)*width + col - 1;
     }
-    
-    
+
+
     //test client
     public static void main(String[] args) {
 //        Percolation p = new Percolation(5);
@@ -101,6 +109,6 @@ public class Percolation {
 //        p.open(4,1);
 //        p.open(5,1);
 //        System.out.println("percolates:" + p.percolates());
-        
+
     }
 }
---
+++ PercolationStats.java
@@ -0,0 +1,67 @@
+//import java.lang.Math;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    private Double meanValue = 0.0;
+    private Double stddevValue = 0.0;
+    private Double confLoValue = 0.0;
+    private Double confHiValue = 0.0;
+
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n < 1 || trials < 1) {
+            throw new IllegalArgumentException();
+        }
+        double[] thresholds = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            while (!p.percolates()) {
+                //choose a site randomly
+                int rowIndex = StdRandom.uniform(n) + 1;
+                int colIndex = StdRandom.uniform(n) + 1;
+                p.open(rowIndex, colIndex);
+            }
+            //it took 'counter' sites to percolate in this trial
+            System.out.println("o: " + p.numberOfOpenSites());
+            //System.out.println("t:" + counter/(n*n));
+            thresholds[i] = (double) p.numberOfOpenSites() / (n*n);
+        }
+        meanValue = StdStats.mean(thresholds);
+        stddevValue = StdStats.stddev(thresholds);
+        confLoValue = meanValue - (1.96*stddevValue / Math.sqrt(trials));
+        confHiValue = meanValue + (1.96*stddevValue / Math.sqrt(trials));
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return meanValue;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return stddevValue;
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return confLoValue;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return confHiValue;
+    }
+
+    // test client (described below)
+    public static void main(String[] args) {
+        PercolationStats p = new PercolationStats(Integer.parseInt(args[0]),
+                                              Integer.parseInt(args[1]));
+        System.out.println("mean   : " + p.mean());
+        System.out.println("stddev : " + p.stddev());
+        System.out.println("conflo : " + p.confidenceLo());
+        System.out.println("confhi : " + p.confidenceHi());
+    }
+}
